b05c7f2813651019d4542f2f73f59a6cafe6b930
devdavinci <dev.muhammad222000@gmail.com>
2023-05-06 00:47:21 +0200

    final project

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,render_template,url_for
+from flask import Flask,request,render_template,url_for, jsonify
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 import joblib as joblib
@@ -9,18 +9,32 @@ scaler=joblib.load('scaler.save')
 
 app =Flask(__name__)
 
-# IMG_FOLDER=os.path.join('static','IMG')
-# app.config['UPLOAD_FOLDER']=IMG_FOLDER
+IMG_FOLDER=os.path.join('static','IMG')
+app.config['UPLOAD_FOLDER']=IMG_FOLDER
 
 
 @app.route('/hi')
 def index():
     return "hello"
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/',methods=['POST'])
 def home():
-    if request.method=='GET':
-        return "Hello from home"
+    req_data=request.get_json()
+    raw_data = {
+        'sl':req_data['sl'],
+        'sw':req_data['sw'],
+        'pl':req_data['pl'],
+        'pw':req_data['pw'],
+    }
+    data = np.array([[raw_data['sl'], raw_data['sw'], raw_data['pl'], raw_data['pw']]])
+    x = scaler.transform(data)
+    print(x)
+    prediction = model.predict(x)
+    print(prediction)
+    image=prediction[0]+'.png'
+    image=os.path.join(app.config['UPLOAD_FOLDER'],image)
+    print(image)
+    return image
 
 
 if __name__ == '__main__':
