8e40aab7163960ffa7c660ea2339a5f49bf69e29
devdavinci <dev.muhammad222000@gmail.com>
2023-05-06 18:44:23 +0200

    changing model

--- app.py
+++ app.py
@@ -14,18 +14,13 @@ app = Flask(__name__)
 
 def preprossing(image):
     image=Image.open(image)
-    image = image.resize((150, 150))
+    image = image.resize((224, 224))
     image_arr = np.array(image.convert('RGB'))
-    image_arr.shape = (1, 150, 150, 3)
+    image_arr.shape = (1, 224, 224, 3)
     return image_arr
 
-classes = ['Buildings' ,'Forest', 'Glacier' ,'Mountain' ,'Sea' ,'Street']
-model=load_model("Intel_Image_Classification.h5")
-
-@app.route('/')
-def index():
-
-    return "hello"
+classes = ["Positive","Negative"]
+model=load_model("covid19.h5")
 
 
 @app.route('/predictApi', methods=["POST"])
@@ -38,12 +33,46 @@ def api():
         image_arr = preprossing(image)
         print("Model predicting ...")
         result = model.predict(image_arr)
-        print("Model predicted", result)
+        print("Model predicted",result)
         ind = np.argmax(result)
         prediction = classes[ind]
         print(prediction)
         return jsonify({'prediction': prediction})
-    except Exception as e: print(e)
+    except:
+        return jsonify({'Error': 'Error occur'})
+
+# def preprossing(image):
+#     image=Image.open(image)
+#     image = image.resize((150, 150))
+#     image_arr = np.array(image.convert('RGB'))
+#     image_arr.shape = (1, 150, 150, 3)
+#     return image_arr
+
+# classes = ['Buildings' ,'Forest', 'Glacier' ,'Mountain' ,'Sea' ,'Street']
+# model=load_model("covid19_kh_abdo.h5")
+
+# @app.route('/')
+# def index():
+
+#     return "hello"
+
+
+# @app.route('/predictApi', methods=["POST"])
+# def api():
+#     # Get the image from post request
+#     try:
+#         if 'fileup' not in request.files:
+#             return "Please try again. The Image doesn't exist"
+#         image = request.files.get('fileup')
+#         image_arr = preprossing(image)
+#         print("Model predicting ...")
+#         result = model.predict(image_arr)
+#         print("Model predicted", result)
+#         ind = np.argmax(result)
+#         prediction = classes[ind]
+#         print(prediction)
+#         return jsonify({'prediction': result})
+#     except Exception as e: print(e)
 
 
 
