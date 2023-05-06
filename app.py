145ef138a3428823d99db1abfb59f8d7e71f2845
devdavinci <dev.muhammad222000@gmail.com>
2023-05-06 19:06:52 +0200

    cleaning up

--- app.py
+++ app.py
@@ -47,39 +47,6 @@ def api():
     except:
         return jsonify({'Error': 'Error occur'})
 
-# def preprossing(image):
-#     image=Image.open(image)
-#     image = image.resize((150, 150))
-#     image_arr = np.array(image.convert('RGB'))
-#     image_arr.shape = (1, 150, 150, 3)
-#     return image_arr
-
-# classes = ['Buildings' ,'Forest', 'Glacier' ,'Mountain' ,'Sea' ,'Street']
-# model=load_model("covid19_kh_abdo.h5")
-
-# @app.route('/')
-# def index():
-
-#     return "hello"
-
-
-# @app.route('/predictApi', methods=["POST"])
-# def api():
-#     # Get the image from post request
-#     try:
-#         if 'fileup' not in request.files:
-#             return "Please try again. The Image doesn't exist"
-#         image = request.files.get('fileup')
-#         image_arr = preprossing(image)
-#         print("Model predicting ...")
-#         result = model.predict(image_arr)
-#         print("Model predicted", result)
-#         ind = np.argmax(result)
-#         prediction = classes[ind]
-#         print(prediction)
-#         return jsonify({'prediction': result})
-#     except Exception as e: print(e)
-
 
 
 if __name__ == '__main__':
